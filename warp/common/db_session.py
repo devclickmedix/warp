592f47ead83334872d6422eedc837228de91df45
Dat Nguyen <datnt@anymindgroup.com>
2019-06-26 15:38:07 +0700

    fix typo

--- warp/common/db_session.py
+++ warp/common/db_session.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 def setupSession():
     conn_str = config['db'].replace("postgres", "postgresql")
     engine = create_engine(conn_str, 
-        isolation_level=config.get("isolation_level", "READ COMMITTED")
+        isolation_level=config.get("isolation_level", "READ COMMITTED"),
         echo=config.get("trace"))
     Session = sessionmaker(bind=engine)
     return Session()
