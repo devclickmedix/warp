44eb96fb35837a37189aaaf7c11fff058bc6a9e9
Dat Nguyen <datnt@anymindgroup.com>
2019-07-16 12:05:31 +0700

    remote extra param on db connection string

--- warp/common/db_session.py
+++ warp/common/db_session.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import sessionmaker
 
 def setupSession():
     conn_str = config['db'].replace("postgres", "postgresql")
+    # check if is there any option for storm
+    conn_str = conn_str.split("?")[0]
     engine = create_engine(conn_str, 
         isolation_level=config.get("isolation_level", "READ COMMITTED"),
         echo=config.get("trace"))
